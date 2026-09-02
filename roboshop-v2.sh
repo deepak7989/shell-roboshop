525bba99c1691474e9cde8b7a63176c9bdb77aaa
deepak7989 <deepakvadla05@gmail.com>
2026-09-02 17:11:37 +0530

    api doc added

--- roboshop-v2.sh
+++ roboshop-v2.sh
@@ -78,5 +78,12 @@ do
             ]
         }'
         echo "updated R53 record for: $instance"
+    else
+        if [ $INSTANCE_ID == "None" ]; then
+            echo "$instance alredy destroyed, nothing to do..."
+        else
+            aws ec2 terminate-instances --instance-ids $INSTANCE_ID
+            echo "Terminating Instance: $instance"
+        fi 
     fi
 done
