d71a1b3d0626995189d281a9dfc768280a48693e
deepak7989 <deepakvadla05@gmail.com>
2026-09-02 15:43:12 +0530

    api doc added

--- roboshop-v2.sh
+++ roboshop-v2.sh
@@ -24,3 +24,28 @@ if [ "$ACTION" != "create" ] && [ "$ACTION" != "delete" ]; then
     exit 1
 fi
 
+get_instance_id(){
+    name=$1
+    aws ec2 describe-instances --filters "Name=tag:Name,Values=roboshop" "Name=instance-state-name,Values=running" --query  "Reservations[0].Instances[0].InstanceId" --output text
+}
+
+for instanvce in $@
+do
+    INSTANCE_ID=$(get_instance_id $instance)
+    if [ $ACTION == "create" ]; then
+        if [ $INSTANCE_ID == "None" ]; then
+            echo "Launching Instance: roboshop-$instance"
+            $INSTANCE_ID=$( aws ec2 run-instances \
+            --image-id $AMI_ID \
+            --instance-type t3.micro \
+            --security-groups "roboshop-common" "roboshop-$instance" \
+            --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value="roboshop-$instance"}]" \
+            --query 'Instances[0].InstanceId' \
+            --output text 
+            )
+            echo "Launched Instnace: $INSTANCE_ID"
+        else
+            echo "roboshop-$instance alredy running: $INSTANCE_ID"
+        fi
+    fi
+done
