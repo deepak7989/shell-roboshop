74d9d9162a3a3f3e4c84a1664f0f9f9fe7adca76
deepak7989 <deepakvadla05@gmail.com>
2026-09-02 23:23:22 +0530

    api doc added

--- 02-redis.sh
+++ 02-redis.sh
@@ -32,7 +32,7 @@ dnf module enable redis:7 -y &>> $LOGS_FILE
 dnf install redis -y &>> $LOGS_FILE
 VALIDATE $? "Installin Redis:7"
 
-sed -i -e 's/127.0.0.1/0.0.0.0/g' -e '/protectec-mode/ c protectec-mode no' /etc/redis/redis.conf
+sed -i -e 's/127.0.0.1/0.0.0.0/g' -e 's/protected-mode yes/protected-mode no/g' /etc/redis/redis.conf &>> $LOGS_FILE
 VALIDATE $? "Allowing remote coonections"
 
 systemctl enable redis &>> $LOGS_FILE
