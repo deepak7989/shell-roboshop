2437953aab7f772f6318d9b157d9e343e975a1e7
deepak7989 <deepakvadla05@gmail.com>
2026-09-02 18:01:56 +0530

    api doc added

---
+++ 06-user.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+LOG_FOLDER="/var/log/roboshop" #creating folder #in this folder mongodb.sh will be created
+sudo mkdir -p $LOG_FOLDER #inside var/log
+sudo chown -R ec2-user:ec2-user $LOG_FOLDER # Set permission to ec2-user
+sudo chmod -R 755 $LOG_FOLDER
+LOGS_FILE="$LOG_FOLDER/$0.log"
+SCRIPT_DIR=$PWD
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+TIMESTAMP=$(date "+%Y-%m-%d %H:%M:%S")
+
+if [ $USERID -ne 0 ]; then
+    echo -e "$TIMESTAMP [ERROR] $R Please run this script with root access $N" | tee -a $LOGS_FILE
+    exit 1
+fi
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+        echo -e "$TIMESTAMP [ERROR] $2 ... $R FAILURE $N" | tee -a $LOGS_FILE
+        exit 1
+    else
+        echo -e "$TIMESTAMP [INFO] $2 ... $G SUCCESS $N" | tee -a $LOGS_FILE
+    fi
+}
+
+dnf module disable nodejs -y &>> $LOGS_FILE
+dnf module enable nodejs:20 -y &>> $LOGS_FILE
+dnf install nodejs -y &>> $LOGS_FILE
+VALIDATE $? "Instaling NodeJS:20"
+
+id roboshop &>> $LOGS_FILE
+if [ $? -ne 0 ]; then
+    useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>> $LOGS_FILE
+    VALIDATE $? "Creating roboshop system user"
+else
+    echo -e "System user roboshop alredy created ... $Y SKIPPING $N"
+fi
+
+rm -rf /app
+VALIDATE $? "Removing existing code"
+
+rm -rf /tmp/user.zip
+VALIDATE $? "Removed user zip"
+
+mkdir -p /app &>> $LOGS_FILE
+VALIDATE $? "Creating app directory"
+
+curl -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user-v3.zip &>> $LOGS_FILE
+cd /app 
+unzip /tmp/user.zip &>> $LOGS_FILE
+VALIDATE $? "Dowloaded and extracted user code"
+
+npm install &>> $LOGS_FILE
+VALIDATE $? "Installing dependencies"
+
+cp $SCRIPT_DIR/user.service /etc/systemd/system/user.service
+VALIDATE $? "Created systemctl service"
+
+systemctl enable user &>> $LOGS_FILE
+systemctl restart user &>> $LOGS_FILE
+VALIDATE $? "Restarting user"
